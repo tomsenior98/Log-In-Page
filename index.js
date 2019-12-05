7677e2ea329f266acdf63fbc5c7e2e1291db0abc
tomsenior20 <tomsenior>
2019-12-05 15:39:22 +0000

    log in page completed

---
+++ index.js
@@ -0,0 +1,23 @@
+var attempt = 3;
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "tsenior18" && password == "password"){
+alert ("Login successfully");
+window.location = "welcome.html"; // Redirecting to other page.
+return false;
+}
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
+}
+
+
