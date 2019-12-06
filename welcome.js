42e0320ac143ef86bafdc0e5c96935d9731dce6b
tomsenior20 <tomsenior>
2019-12-06 09:28:11 +0000

    Updated login color scheme

--- welcome.js
+++
@@ -1,7 +0,0 @@
-function getDate() {
-var date = new Date();
-var h = date.getHours();
-var m = date.getMinutes();
-var s = date.getSeconds();
-document.getElementById("times").innerHTML = h + ":" + m + ":" + s;
-}
